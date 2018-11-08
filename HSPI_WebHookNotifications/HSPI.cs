845864e099724c5a6b8ed31b354bd5351f50e577
Alex Corn <mckay@doctormckay.com>
2018-11-08 03:29:14 -0500

    Added settings page

--- HSPI_WebHookNotifications/HSPI.cs
+++ HSPI_WebHookNotifications/HSPI.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Net.Http;
 using System.Text;
+using System.Web;
 using System.Web.Script.Serialization;
 using HomeSeerAPI;
 using Scheduler;
@@ -11,7 +13,7 @@ namespace HSPI_WebHookNotifications
 {
 	public class HSPI : HspiBase
 	{
-		private string webhookEndpoint = "http://192.168.1.10:19533/hs3_event";
+		private string webhookEndpoint;
 		private readonly HttpClient httpClient;
 		private readonly JavaScriptSerializer jsonSerializer;
 		
@@ -25,15 +27,36 @@ namespace HSPI_WebHookNotifications
 
 		public override string InitIO(string port) {
 			Program.WriteLog("verbose", "InitIO");
+
+			webhookEndpoint = hs.GetINISetting("Config", "webhook_endpoint", "", IniFilename);
 			
 			callbacks.RegisterEventCB(Enums.HSEvent.VALUE_SET, Name, InstanceFriendlyName());
 			callbacks.RegisterEventCB(Enums.HSEvent.VALUE_CHANGE, Name, InstanceFriendlyName());
 			callbacks.RegisterEventCB(Enums.HSEvent.STRING_CHANGE, Name, InstanceFriendlyName());
+			
+			// Config page
+			hs.RegisterPage("WebHookNotificationConfig", Name, InstanceFriendlyName());
+			WebPageDesc configLink = new WebPageDesc {
+				plugInName = Name,
+				plugInInstance = InstanceFriendlyName(),
+				link = "WebHookNotificationConfig",
+				linktext = "Settings",
+				order = 1,
+				page_title = "WebHook Notifications Settings"
+			};
+			callbacks.RegisterConfigLink(configLink);
+			callbacks.RegisterLink(configLink);
 
 			return "";
 		}
 
 		public override void HSEvent(Enums.HSEvent eventType, object[] parameters) {
+			if (string.IsNullOrEmpty(webhookEndpoint)) {
+				Program.WriteLog("debug",
+					"Ignoring event " + eventType + " because no webhook endpoint is configured.");
+				return;
+			}
+			
 			Dictionary<string, object> dict = new Dictionary<string, object> {
 				{"eventType", eventType.ToString()}
 			};
@@ -62,11 +85,85 @@ namespace HSPI_WebHookNotifications
 					Content = new StringContent(json, Encoding.UTF8, "application/json")
 				};
 
-				httpClient.SendAsync(req).ContinueWith((task) => { task.Result.Dispose(); });
+				httpClient.SendAsync(req).ContinueWith((task) => {
+					if (!task.Result.IsSuccessStatusCode) {
+						Program.WriteLog("warn", "Got non-successful response code from WebHook: " + task.Result.StatusCode);
+					}
+					
+					task.Result.Dispose();
+				});
 			}
 			catch (Exception ex) {
 				Program.WriteLog("error", ex.ToString());
 			}
+		}
+
+		public override string GetPagePlugin(string pageName, string user, int userRights, string queryString) {
+			Program.WriteLog("debug", "Requested page name " + pageName + " by user " + user + " with rights " + userRights);
+			if (pageName != "WebHookNotificationConfig") {
+				return "Unknown page " + pageName;
+			}
+
+			if ((userRights & 2) != 2) {
+				// User is not an admin
+				return "Access denied: you are not an administrative user.";
+			}
+
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append(
+				PageBuilderAndMenu.clsPageBuilder.FormStart("whn_config_form", "whn_config_form", "post"));
+
+			stringBuilder.Append(
+				"<table width=\"1000px\" cellspacing=\"0\"><tr><td class=\"tableheader\" colspan=\"2\">Settings</td></tr>");
+			stringBuilder.Append(
+				"<tr><td class=\"tablecell\" colspan=\"1\" style=\"width:300px\" align=\"left\">WebHook URL:</td>");
+			stringBuilder.Append("<td class=\"tablecell\" colspan=\"1\">");
+			
+			clsJQuery.jqTextBox textBox = new clsJQuery.jqTextBox("WebHookUrl", "text", webhookEndpoint, pageName, 100, true);
+			stringBuilder.Append(textBox.Build());
+			stringBuilder.Append("</td></tr></table>");
+
+			clsJQuery.jqButton doneBtn = new clsJQuery.jqButton("DoneBtn", "Done", pageName, false);
+			doneBtn.url = "/";
+			stringBuilder.Append("<br />");
+			stringBuilder.Append(doneBtn.Build());
+			stringBuilder.Append("<br /><br />");
+			
+			PageBuilderAndMenu.clsPageBuilder pageBuilder = new PageBuilderAndMenu.clsPageBuilder(pageName);
+			pageBuilder.reset();
+			pageBuilder.AddTitleBar("WebHook Notifications Settings", user, true, "", false, false, false, false);
+			pageBuilder.AddHeader(hs.GetPageHeader(pageName, "WebHook Notifications Settings", "", "", false, true));
+			pageBuilder.AddBody(stringBuilder.ToString());
+			pageBuilder.AddFooter(hs.GetPageFooter());
+			pageBuilder.suppressDefaultFooter = true;
+
+			return pageBuilder.BuildPage();
+		}
+		
+		public override string PostBackProc(string pageName, string data, string user, int userRights) {
+			Program.WriteLog("debug", "PostBackProc page name " + pageName + " by user " + user + " with rights " + userRights);
+			if (pageName != "WebHookNotificationConfig") {
+				return "Unknown page " + pageName;
+			}
+
+			if ((userRights & 2) != 2) {
+				// User is not an admin
+				return "Access denied: you are not an administrative user.";
+			}
+
+			try {
+				NameValueCollection postData = HttpUtility.ParseQueryString(data);
+				string url = postData.Get("WebHookUrl");
+				if (url != null) {
+					webhookEndpoint = url;
+					hs.SaveINISetting("Config", "webhook_endpoint", url, IniFilename);
+					Program.WriteLog("info", "Saved new WebHook URL " + url);
+				}
+			} catch (Exception ex) {
+				Program.WriteLog("warn", ex.ToString());
+			}
+
+			return "";
 		}
 	}
 }
